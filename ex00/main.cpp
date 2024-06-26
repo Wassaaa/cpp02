c405acb78aaa82e231f3e0082e2c29a654df349f
aklein <aklein@student.hive.fi>
2024-06-26 18:40:06 +0300

    ro5

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,7 +2,7 @@
 #include <utility>
 
 Fixed createTemporary() {
-    return Fixed();
+	return Fixed();
 }
 
 int main( void ) {
@@ -15,22 +15,22 @@ int main( void ) {
 	std::cout << c.getRawBits() << std::endl;
 
 	std::cout << "\nTesting move constructor:" << std::endl;
-    Fixed d = createTemporary();
-    std::cout << d.getRawBits() << std::endl;
+	Fixed d = createTemporary();
+	std::cout << d.getRawBits() << std::endl;
 
-    std::cout << "\nTesting move assignment:" << std::endl;
-    Fixed e;
-    e = createTemporary();
-    std::cout << e.getRawBits() << std::endl;
+	std::cout << "\nTesting move assignment:" << std::endl;
+	Fixed e;
+	e = createTemporary();
+	std::cout << e.getRawBits() << std::endl;
 
-    std::cout << "\nTesting explicit moves:" << std::endl;
-    Fixed f = std::move(a);
-    std::cout << f.getRawBits() << std::endl;
-    std::cout << "a after move: " << a.getRawBits() << std::endl;
+	std::cout << "\nTesting explicit moves:" << std::endl;
+	Fixed f = std::move(a);
+	std::cout << f.getRawBits() << std::endl;
+	std::cout << "a after move: " << a.getRawBits() << std::endl;
 
-    Fixed g;
-    g = std::move(b);
-    std::cout << g.getRawBits() << std::endl;
-    std::cout << "b after move: " << b.getRawBits() << std::endl;
+	Fixed g;
+	g = std::move(b);
+	std::cout << g.getRawBits() << std::endl;
+	std::cout << "b after move: " << b.getRawBits() << std::endl;
 	return 0;
 }
